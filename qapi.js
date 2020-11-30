524e680affb3600c00404a4c26cd64b384a8d9d8
jesbin16 <68122575+jesbin16@users.noreply.github.com>
2020-11-30 16:33:48 +0530

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fc4d15f79530b001fd2e3be",
+				"5fc4d15f79530b001fd2e3c0"
+			]
+		},
+		"5fc4d15f79530b001fd2e3be": {
+			"id": "5fc4d15f79530b001fd2e3be",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fc4d15f79530b001fd2e3c0": {
+			"id": "5fc4d15f79530b001fd2e3c0",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
